f76cf453010d86bb89959d4272b8fe5e1f30ffef
Alexander Podkopaev <alexander@podkopaev.net>
2023-01-11 11:50:37 +0300

    Added template of map_reduce function

--- weak_5_task_1.1/main.cpp
+++ weak_5_task_1.1/main.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+#include <future>
+
+using namespace std;
+
+template<typename Iter, typename Func1, typename Func2>
+auto map_reduce(Iter p, Iter q, Func1 f1, Func2 f2, size_t threads)
+{
+
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
